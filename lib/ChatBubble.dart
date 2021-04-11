ad41d526f23b81cb0b7da7b723f7b0c4bc8bf6e8
Theodore Li <theodoreqili@gmail.com>
2021-04-11 09:30:17 -0500

    Fix chatbubble overflow

--- lib/ChatBubble.dart
+++ lib/ChatBubble.dart
@@ -9,21 +9,24 @@ class ChatBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(children: <Widget>[
       isUser ? Spacer() : Container(),
-      Container(
-        padding: EdgeInsets.all(12),
-        margin: EdgeInsets.only(left: 14, right: 14, top: 14),
-        decoration: BoxDecoration(
-          border: Border.all(width: 2, color: Colors.blue),
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-              bottomLeft: isUser ? Radius.circular(20) : Radius.circular(0),
-              bottomRight: !isUser ? Radius.circular(20) : Radius.circular(0)),
-          color: isUser ? Colors.blue : Colors.white,
-        ),
-        child: Text(
-          text,
-          style: TextStyle(color: isUser ? Colors.white : Colors.blue),
+      Flexible(
+        child: Container(
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.only(left: 14, right: 14, top: 14),
+          decoration: BoxDecoration(
+            border: Border.all(width: 2, color: Colors.blue),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+                bottomLeft: isUser ? Radius.circular(20) : Radius.circular(0),
+                bottomRight:
+                    !isUser ? Radius.circular(20) : Radius.circular(0)),
+            color: isUser ? Colors.blue : Colors.white,
+          ),
+          child: Text(
+            text,
+            style: TextStyle(color: isUser ? Colors.white : Colors.blue),
+          ),
         ),
       ),
       !isUser ? Spacer() : Container(),
